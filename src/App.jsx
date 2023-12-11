dd33e2bcb609b6cbfaf2e4fb5698a689ce0819f6
satyadeep <satyadeepjandhyam@gmail.com>
2023-12-11 10:51:21 +0530

    changed home to Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import './App.css';
 import{BrowserRouter,Routes,Route} from 'react-router-dom'
-import Home from './pages/home';
+import Home from './pages/Home';
 import About from './pages/About';
 import AdobeXD from './pages/technologies/AdobeXD';
 import Aws from './pages/technologies/Aws';
